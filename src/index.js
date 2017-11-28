a7e0f123e70dc86b98f92cc9b3f7538e35d323c9
Kien Vu <kienvu@usit.uio.no>
2017-11-28 13:09:00 +0100

    MOB-1221: removed ListElement from library

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 // components
 export { default as Button } from './Button';
-export { default as ListElement } from './ListElement';
 export { default as MultipleOptionList } from './MultipleOptionList';
 export { default as SingleOptionList } from './SingleOptionList';
