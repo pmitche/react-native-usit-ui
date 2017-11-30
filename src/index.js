3a052cda34956e1d334fdf08729adc351b6a52ae
Kien Vu <kienvu@usit.uio.no>
2017-11-30 12:16:16 +0100

    MOB-1239: added UiOFooter

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ export { default as Button } from './Button';
 export { default as MultipleOptionList } from './MultipleOptionList';
 export { default as SingleOptionList } from './SingleOptionList';
 export { default as MenuButton } from './MenuButton';
+export { default as UiOFooter } from './UiOFooter';
