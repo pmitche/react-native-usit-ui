db2e6f15ccbe39eec19c4d1308aced5e586e5ef3
Kien Vu <kienvu@usit.uio.no>
2017-12-12 09:20:34 +0100

    MOB-1265: fixed after PR comments

--- src/Accordion/Accordion.js
+++ src/Accordion/Accordion.js
@@ -1,7 +1,6 @@
 // @flow
 import React, { Component } from 'react';
 import {
-  Animated,
   Dimensions,
   StyleSheet,
   LayoutAnimation,
@@ -10,12 +9,11 @@ import {
   View,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import Divider from '../Divider'
+import Divider from '../Divider';
 
 import { colors } from '../styles';
 
 const { width } = Dimensions.get('window');
-const AnimatedIcon = Animated.createAnimatedComponent(Icon);
 
 type AccordionProps = {
   title: string,
@@ -33,10 +31,10 @@ type AccordionState = {
 class Accordion extends Component<AccordionProps, AccordionState> {
   static defaultProps = {
     title: 'Title',
-    content: '',
+    content: 'Placeholder',
     color: colors.primary,
     iconSize: 35,
-  }
+  };
 
   constructor() {
     super();
@@ -50,7 +48,15 @@ class Accordion extends Component<AccordionProps, AccordionState> {
   }
 
   render() {
-    const { title, titleStyle, content, contentStyle, color, iconSize } = this.props
+    const {
+      title,
+      titleStyle,
+      content,
+      contentStyle,
+      color,
+      iconSize,
+    } = this.props;
+
     return (
       <View style={{ width: width * 0.9 }}>
         <TouchableOpacity
@@ -62,18 +68,17 @@ class Accordion extends Component<AccordionProps, AccordionState> {
             <Text style={[{ fontSize: 25 }, titleStyle]}>{title}</Text>
           </View>
           <View style={styles.icon}>
-            <AnimatedIcon
-              name="keyboard-arrow-up"
-              style={{
-                transform: [{ rotate: this.state.active ? '180deg' : '0deg' }],
-              }}
+            <Icon
+              name={
+                this.state.active ? 'keyboard-arrow-up' : 'keyboard-arrow-down'
+              }
               size={iconSize}
               color={color}
             />
           </View>
         </TouchableOpacity>
         {this.state.active && (
-            <Text style={[ { fontSize: 15 }, contentStyle ]}>{content}</Text>
+          <Text style={[{ fontSize: 15 }, contentStyle]}>{content}</Text>
         )}
         <Divider />
       </View>
