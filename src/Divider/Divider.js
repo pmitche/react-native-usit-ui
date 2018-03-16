514362ebec0384825cdd6477114dadf70c9cabc9
Paul Philip Mitchell <paulpmit@gmail.com>
2018-03-16 12:29:53 +0100

    Add vertical and height props to Divider

--- src/Divider/Divider.js
+++ src/Divider/Divider.js
@@ -4,18 +4,31 @@ import { StyleSheet, View, Dimensions } from 'react-native';
 
 const { width } = Dimensions.get('window');
 
-const Divider = ({ style }: { style?: Object }) => (
-  <View
-    style={[
-      {
-        width: width * 0.9,
-        height: 1.5,
-        marginVertical: 15,
-        backgroundColor: '#B6B6B6',
-      },
-      StyleSheet.flatten(style),
-    ]}
-  />
-);
+type Props = {
+  vertical?: boolean,
+  height?: number | string,
+  style?: Object,
+};
+
+const Divider = ({ vertical, height, style }: Props) => {
+  return (
+    <View
+      style={[
+        {
+          width: vertical ? 1.5 : width * 0.9,
+          height: vertical ? height : 1.5,
+          marginVertical: vertical ? 0 : 15,
+          marginHorizontal: vertical ? 15 : 0,
+          backgroundColor: '#B6B6B6',
+        },
+        StyleSheet.flatten(style),
+      ]}
+    />
+  );
+};
+
+Divider.defaultProps = {
+  height: '100%',
+};
 
 export default Divider;
